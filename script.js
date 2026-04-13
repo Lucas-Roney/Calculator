7436c82253c9d7cf0b7cc8b2c0268f0aeee66aa1
roneylj_appstate <roneylj@appstate.edu>
2026-04-13 00:29:12 -0400

    working sin cos and tan along with mode

--- script.js
+++ script.js
@@ -18,8 +18,11 @@ const buttonMap = {
     "clear" : "Escape",
     "x" : "*",
     "÷" : "/",
-    "(-)" : "-",
     "enter" : "Enter",
+    "sin" : "s",
+    "cos" : "c",
+    "tan" : "t",
+    "mode" : "m",
     "" : "",
 }
 
@@ -33,28 +36,61 @@ document.querySelector("#Button-Grid-Container").addEventListener('click', funct
     const label = button.textContent;
     const key = buttonMap[label] ?? label;
     Call_keydown(key);
+    button.blur();
 });
 
 let expression = "";
 let expressionDisp = "";
 let carrot = "";
+let mode = 'radians';
 let result_text = document.querySelector("#Result-Text");
 
 let justEvaluated = false;
 let justDisplayed = '';
 let justExpressed = '';
+let justResulted = '';
 let justCarrot = '';
+let justMode1 = false;
+let justMode2 = false;
+let oldResulted = '';
 
 const display1 = document.querySelector("#Display-Text");
 const display2 = document.querySelector("#Display-Text2");
 let display = display1;
 const divider = document.querySelector("#Display_Divider")
 
-const allowedKeys = ['0','1','2','3','4','5','6','7','8','9','.','+','-','*','/','^','(',')'];
+let currDisplay1 = '';
+let currDisplay2 = '';
+let currResult = '';
+
+const allowedKeys = ['0','1','2','3','4','5','6','7','8','9','.','+','-','*','/','^','(',')', '(-)', 's', 'c', 't', 'm'];
 const operators = ['+','-','*','/','^'];
+
+function countParens(str) {
+    return str.split('(').length - str.split(')').length;
+}
 
 function Call_keydown(key) {
     if (screen.classList.contains("On")) {
+
+        // Check if mode message
+        if (justMode1) {
+            display1.textContent = currDisplay1;
+            cursor1.classList.toggle("Show", true);
+            display = display1;
+            justMode1 = false;
+            return;
+        }
+        if (justMode2) {
+            display1.textContent = currDisplay1;
+            result_text.textContent = currResult;
+            divider.classList.toggle("Show", true);
+            display2.textContent = currDisplay2;
+            cursor2.classList.toggle("Show", true);
+            display = display2;
+            justMode2 = false;
+            return;
+        }
 
         //Start new line if just evaluated
         if (justEvaluated) {
@@ -63,18 +99,82 @@ function Call_keydown(key) {
 
         //Consider operator meaning ans operator 
         if (!expression && justEvaluated && operators.includes(key)) {
-            expression = justExpressed;
+            expression = justResulted;
             expressionDisp = "Ans";
             carrot = justCarrot;
             display.textContent = expressionDisp;
+            oldResulted = justResulted;
         }
 
         // Handle numbers and operators
         if (allowedKeys.includes(key)) {
             if(key == '^') {
-                expressionDisp += '^'
+                expressionDisp += '^';
                 expression += '**';
                 carrot += '^';
+            }
+            else if (key == '(-)') {
+                expressionDisp += '(-';
+                expression += '(-';
+                carrot += '(-';
+            }
+            else if (key == "s") {
+                expressionDisp += '(sin(';
+                expression += '(Math.sin(';
+                carrot += '(sin(';
+            }
+            else if (key == "c") {
+                expressionDisp += '(cos(';
+                expression += '(Math.cos(';
+                carrot += '(cos(';
+            }
+            else if (key == "t") {
+                expressionDisp += '(tan(';
+                expression += '(Math.tan(';
+                carrot += '(tan(';
+            }
+            else if (key == "m") {
+                currDisplay1 = display1.textContent;
+                currDisplay2 = display2.textContent;
+                currResult = result_text.textContent;
+                if (display == display1) {
+                    cursor1.classList.toggle("Show", false);
+                    display1.textContent = '';
+                    if (mode == 'radians') {
+                    mode = 'degrees';
+                    }
+                    else {
+                        mode = 'radians';
+                    }
+                    display.textContent = `mode changed to: ${mode}`;
+                    justMode1 = true;
+                    return;
+                }
+                if (display == display2) {
+                    cursor2.classList.toggle("Show", false);
+                    divider.classList.toggle("Show", false);
+                    display1.textContent = '';
+                    result_text.textContent = '';
+                    display2.textContent = '';
+                    if (mode == 'radians') {
+                    mode = 'degrees';
+                    }
+                    else {
+                        mode = 'radians';
+                    }
+                    display.textContent = `mode changed to: ${mode}`;
+                    justMode2 = true;
+                    return;
+                }
+                if (mode == 'radians') {
+                    mode = 'degrees';
+                }
+                else {
+                    mode = 'radians';
+                }
+                display.textContent = `mode changed to: ${mode}`;
+                justMode = true;
+                return;
             }
             else{
                 expression += key;
@@ -92,10 +192,15 @@ function Call_keydown(key) {
 
         // Handle backspace
         else if (key === 'Backspace') {
-            if (expressionDisp.at(-1) == 's' && expressionDisp.at(-2) == 'n' && expressionDisp.at(-3) == 'A') {
+            if (expressionDisp.at(-1) == 's' && expressionDisp.at(-2) == 'n') {
                 expressionDisp = expressionDisp.slice(0,-3);
-                expression = expression.slice(0,-justExpressed.length);
+                expression = expression.slice(0,-justResulted.length);
                 carrot = carrot.slice(0,-justCarrot.length)
+            }
+            else if (expressionDisp.at(-1) == '(' && expressionDisp.at(-2) == 'n' && expressionDisp.at(-3) == 'i' || expressionDisp.at(-1) == '(' && expressionDisp.at(-2) == 'n' && expressionDisp.at(-3) == 'a' || expressionDisp.at(-1) == '(' && expressionDisp.at(-2) == 's' && expressionDisp.at(-3) == 'o') {
+                expressionDisp = expressionDisp.slice(0,-5);
+                expression = expression.slice(0,-10);
+                carrot = carrot.slice(0,-5)
             }
             if (carrot.at(-1) == '^') {
                 expression = expression.slice(0, -2);
@@ -119,13 +224,32 @@ function Call_keydown(key) {
         else if (key === 'Enter' || key === '=') {
             try {
                 // Evaluate safely
-                const result = Function('"use strict";return (' + expression + ')')();
+                const parens = countParens(expression);
+                for (i = parens; i > 0; i--) {
+                    expression += ')';
+                }
+                expression = expression.replace(/(\d)\(/g, '$1*(');
+                if (mode == 'degrees') {
+                    expression = expression.replace('Math.sin(', 'Math.sin(Math.PI/180*');
+                    expression = expression.replace('Math.cos(', 'Math.cos(Math.PI/180*');
+                    expression = expression.replace('Math.tan(', 'Math.tan(Math.PI/180*');
+                }
+                let result = Function('"use strict";return (' + expression + ')')();
+                result = result.toFixed(10);
+                while (result.at(-1) == '0' || result.at(-1) == '.') {
+                    if (result.at(-1) == '.') {
+                        result = result.slice(0,-1);
+                        break;
+                    }
+                    result = result.slice(0,-1);
+                }
                 result_text.textContent = result;
 
                 if (justEvaluated) {
                     justDisplayed = display.textContent;
-                    display1.textContent = justDisplayed;
-                    justExpressed = String(result);
+                    display1.textContent = display.textContent;
+                    justExpressed = expression;
+                    justResulted = String(result);
                     justCarrot = carrot;
                     carrot = '';
                     expression = '';
@@ -138,7 +262,8 @@ function Call_keydown(key) {
                     divider.classList.toggle("Show", true);
                     justEvaluated = true;
                     justDisplayed = display.textContent;
-                    justExpressed = String(result);
+                    justExpressed = expression;
+                    justResulted = String(result);
                     justCarrot = carrot;
                     carrot = '';
                     expression = '';
@@ -163,6 +288,25 @@ function Call_keydown(key) {
             cursor2.classList.toggle("Show", false);
             cursor1.classList.toggle("Show", true);
             divider.classList.toggle("Show", false);
+        }
+        else if (key === 'ArrowUp' && display == display2  && !justDisplayed.includes('Ans')) {
+            display2.textContent = justDisplayed;
+            expression = justExpressed;
+            expressionDisp = justDisplayed;
+            carrot = justCarrot;
+        }
+        else if (key === 'ArrowUp' && display == display2  && justDisplayed.includes('Ans')) {
+            display2.textContent = justDisplayed;
+            expression = justExpressed.replace(oldResulted, justResulted);
+            oldResulted = justResulted
+            expressionDisp = justDisplayed;
+            carrot = justCarrot;
+        }
+        else if (key === 'ArrowDown' && display == display2) {
+            display2.textContent = '';
+            expression = '';
+            expressionDisp = '';
+            carrot = '';
         }
     }
 }
